0835d673a9a6764fc6a1bef706d34cdad62b1bbb
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-07-28 13:08:38 +0100

    fix others tables

--- database scripts/01_modular_base.sql
+++ database scripts/01_modular_base.sql
@@ -180,7 +180,7 @@ CREATE TABLE "public"."core_User" (
     "t_user_id" "uuid",
     claim_admin_id integer,
     officer_id integer,
-    "LegacyID" char(32),
+    "LegacyID" "uuid",
     "ValidityFrom" timestamp with time zone NOT NULL,
     "ValidityTo" timestamp with time zone
 );
@@ -271,7 +271,7 @@ ALTER SEQUENCE "public"."core_User_user_permissions_id_seq" OWNED BY "public"."c
 create table "public"."core_UserMutation"
 (
     id          uuid primary key,
-    core_user_id   integer not null,
+    core_user_id   "uuid" not null,
     mutation_id uuid not null
 );
 
